52b00fba544385d4c56ee9f08d46a4a9623da9b7
Gael Crova <gcrova@Gaels-MB-Pro.local>
2024-07-01 10:56:54 +0200

    23.0.2-IF006

--- scripts/constants.sh
+++ scripts/constants.sh
@@ -15,5 +15,5 @@ licensing_catalog_image="icr.io/cpopen/ibm-licensing-catalog@sha256:dfdd38cac150
 cert_manager_catalog_image="icr.io/cpopen/ibm-cert-manager-operator-catalog@sha256:955732299dd174524612ec8e8076237a491cedee1264e4e4be39c2a92f48bc39" # IBM Certificate Manager 4.2.2 from https://github.com/IBM/cloud-pak/tree/master/repo/case/ibm-cert-manager/4.2.2
 
 cs_catalog_image="icr.io/cpopen/ibm-common-service-catalog@sha256:b6a84f9c99b2c522194f1bfe35187f360f5c69f75784fbc39a443cf867d29c56" # IBM Cloud Foundational Services 4.5 from https://github.com/IBM/cloud-pak/tree/master/repo/case/ibm-cp-common-services/4.5.0
-ads_catalog_image="icr.io/cpopen/ibm-ads-operator-catalog@sha256:ef6727d1ef50258c9451f2c78508ba56cdb9179e08cacf7d369cae47f9c5d809" # 23.0.2-IF005
+ads_catalog_image="icr.io/cpopen/ibm-ads-operator-catalog@sha256:8a001c24ba5d56dee24d11ea823b6f681094c77534b741f3f09eb6822cea5c0a" # 23.0.2-IF006
 edb_catalog_image="icr.io/cpopen/ibm-cpd-cloud-native-postgresql-operator-catalog@sha256:c96aa2e6bce92f2e5e4874116cf1cc1cdd60676499cd04ab1631462b8b883357" # Cloud Native PostgresSQL 4.18.0 from https://github.com/IBM/cloud-pak/tree/master/repo/case/ibm-cloud-native-postgresql/4.18.0
